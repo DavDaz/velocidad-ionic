2de0f26b11a1e5a8075ff3a028d667fe2520e557
DaviDiaz <dadiaz@mides.gob.pa>
2024-09-02 15:43:52 -0500

    Actualizacion de proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app_rsh',
+  webDir: 'www'
+};
+
+export default config;
